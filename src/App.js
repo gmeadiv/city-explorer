dc1b218f27a0bb030a59c5802c27ab107cf1a6ff
George Mead <gmeadiv@gmail.com>
2021-09-20 17:35:20 -0700

    done with lab6

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 
 import React from 'react';
 import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchQuery: '',
-      location: {},
-      latitude: {},
-      longitude: {},
+      location: '',
+      latitude: '',
+      longitude: '',
       error: false,
     }
   }
 
   getLocation = async () => {
-
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+    const locationURL = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
 
     try {
+      const response = await axios.get(locationURL);
+      const location = response.data[0].display_name;
+      const latitude = response.data[0].lat;
+      const longitude = response.data[0].lon;
 
-      const response = await axios.get(url);
-
-      const location = response.data[0];
-      const latitude = response.data[1];
-      const longitude = response.data[2];
+      console.log(latitude, longitude, 'CONSOLE LOG')
 
       this.setState({
         location, // or location:location
@@ -33,34 +35,44 @@ class App extends React.Component {
         error: false,
       });
 
+      // this.showMap();
+
     } catch (error) {
 
-      console.error('Unable to find city', this.state.searchQuery);
+      console.error('Unable to find city', 
+      this.state.searchQuery);
 
       this.setState({ error: true });
     }
-
-
   }
 
   render() {
+
     return (
       <>
         <input onChange={(event) => this.setState({ searchQuery: event.target.value })} placeholder="search for a city"></input>
-        <button onClick={this.getLocation}>Explore!</button>
+        <Button onClick={this.getLocation} variant="warning">Explore!</Button>
 
         {/* falsy: false, 0, 0.0, null, undefined, NaN, '' */}
 
-        {this.state.location.place_id &&
+        {this.state.location &&
+          <>
           <ul>
           <li>City Name: {this.state.location.display_name}</li>
-          <li>Latitude: {this.state.location.lat}</li>
-          <li>Longitude: {this.state.location.lon}</li>
+          <li>Latitude: {this.state.latitude}</li>
+          <li>Longitude: {this.state.longitude}</li>
           </ul>
+          <h3>Map:</h3>
+          <Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.429fe5d05e82b46d41d445914dfbab1b&center=${this.state.latitude},${this.state.longitude}&zoom=12`} alt={this.state.location.display_name} rounded />
+          </>
         }
 
         {
-          this.state.error && <h2>Oh noes!!!</h2>
+          this.state.error &&
+          <Container>
+          <h2>Oh No Pinocchio!</h2>
+          <Image src={`https://upload.wikimedia.org/wikipedia/commons/9/94/Pinocchio_1940.jpg`} alt="A Picture of Pinocchio" rounded fluid/>
+          </Container> 
         }
       </>
     )
