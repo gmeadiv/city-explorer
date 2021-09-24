ec583db0ad5526603ea688bcaaeb43309d93c63b
George Mead <gmeadiv@gmail.com>
2021-09-24 16:25:01 -0700

    adds movie component and renders images

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,12 +5,23 @@ import Row from 'react-bootstrap/Row';
 
 class Movie extends React.Component {
   render() {
-    console.log(this.props, '<---- THIS DOT PROPS TWO')
-    const movies = this.props;
+    const movies = this.props.movies;
+    console.log(movies, '<---- SECOND MOVIE ARRAY LOG')
     return (
       <Container>
+        <h3>MOVIE PLACEHOLDER</h3>
         <Row xs={1} sm={2} md={3} lg={4}>
-          {movies.map((movie, index) => (<Col key={index}><Movie title={movie.original_title} overview={movie.overview} avgVotes={movie.vote_average} totalVotes={movie.vote_count} popularity={movie.popularity} releaseDate={movie.release_date}/>
+          {movies.map((movie, index) => (
+          <Col key={index}>
+            <h4>{movie.original_title}</h4>
+            <p>{movie.overview}</p>
+            <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="this movie's poster" rounded="true" fluid="true" />
+            <ul>
+             <li>Average Vote: {movie.vote_average}</li>
+             <li>Total Votes: {movie.vote_count} </li>
+             <li>Popularity: {movie.popularity}</li> 
+             <li>Release Date: {movie.release_date}</li>
+            </ul>
           </Col>))}
         </Row>
       </Container>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import Movie from './Movie.js';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 
 class Movies extends React.Component {
   render() {
+    const movieArray = this.props.movies;
     return (
-      <></>
+      <Container>
+        <Movie movies={movieArray}/>
+      </Container>
     )
   }
 }
 
 export default Movies;
-
-
-{/* <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="this movie's poster" rounded="true" fluid="true" /> */}
